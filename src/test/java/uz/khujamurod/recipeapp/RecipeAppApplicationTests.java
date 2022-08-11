9a21bbaf87583c044f7771e5f6ffff56836d4b2b
Khujamurod <mr.khujamurod.97@mail.ru>
2022-08-11 21:42:55 +0500

    initial commit.

---
+++ src/test/java/uz/khujamurod/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.khujamurod.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
